c62b91cd9cd9110802f970316524b6bbf1c5de95
jinee <coated.jinee@gmail.com>
2025-01-16 09:39:20 +0900

    fix: 비밀번호 찾기 메일전송

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import PasswordResetForm
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def send_mail(self, subject_template_name, email_template_name,
+                 context, from_email, to_email, html_email_template_name=None):
+        """
+        Override send_mail method to use custom from_email
+        """
+        from_email = 'Codeve <no-reply@codeve.com>'  # 여기서 발신자 주소를 강제로 설정
+        super().send_mail(
+            subject_template_name=subject_template_name,
+            email_template_name=email_template_name,
+            context=context,
+            from_email=from_email,
+            to_email=to_email,
+            html_email_template_name=html_email_template_name
+        ) 
